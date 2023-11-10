091746df52b0b65d941536fc9bc52b090ae7f04d
Ivan-Peshev <91033827+Ivan-Peshev@users.noreply.github.com>
2023-11-10 21:15:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 23:09:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+    
+  // Find the time difference between now and the countdown date
+  var distance = countDownDate - now;
+    
+  // Calculate the weeks, days, hours, minutes and seconds
+  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Display the result in the element with id="countdown"
+  document.getElementById("weeks").innerHTML = weeks;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+    
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
